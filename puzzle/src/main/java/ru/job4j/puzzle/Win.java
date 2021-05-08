b22a47d260d92325b77c7c86c534abecfeeab8e1
DizFix <my_name_is_ilya@mail.ru>
2021-05-08 13:15:17 +0300

    2. Сокобан. Тестовое задание.[75283#292811]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -24,12 +24,10 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int index = 0; index < board.length; index++) {
-            if (board[index][index] == 1) {
                 if (Win.monoHorizontal(board, index) || Win.monoVertical(board, index)) {
                     rsl = true;
                     break;
                 }
-            }
         }
         return rsl;
     }
